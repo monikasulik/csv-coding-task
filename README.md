8a7e5e6f8432e771c9dace5cb4fe276df59f62bc
Monika Sulik <kontakt@monikasulik.net>
2018-04-17 03:15:23 +0100

    Update README

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # csv-coding-task
-A coding task ahead of a job interview
+
+## Installation
+
+  1. cd into the csv-coding-task directory in the command line
+  1. run `pip install -r requirements.txt`
+
+## Running the code
+
+  1. cd into the csv-coding-task directory in the command line
+  1. run `python parse.py`
+
+## Running tests
+
+  1. cd into the csv-coding-task directory in the command line
+  1. run `pytest`
---
+++ parse.py
@@ -0,0 +1,22 @@
+import os
+
+from csv_parser import Parser
+
+
+CSV_FILE_DIR = os.path.join(
+    os.path.dirname( __file__ ), 'csv_parser', 'data')
+
+
+print('----------------------------------------------------')
+print('Results of parsing 1.csv:')
+with open(os.path.join(CSV_FILE_DIR, '1.csv')) as csv_file:
+    print(Parser(csv_file).parse())
+print('----------------------------------------------------')
+print('Results of parsing 2.csv:')
+with open(os.path.join(CSV_FILE_DIR, '2.csv')) as csv_file:
+    print(Parser(csv_file).parse())
+print('----------------------------------------------------')
+print('Results of parsing 3.csv:')
+with open(os.path.join(CSV_FILE_DIR, '3.csv')) as csv_file:
+    print(Parser(csv_file).parse())
+print('----------------------------------------------------')
